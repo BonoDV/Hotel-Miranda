c7ca47b74400f0cde4fdbabec3796a1d969ac23c
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-04-19 21:28:16 +0200

    style: done menu section mobile, photo section mobile

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const roomsSwiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -16,6 +16,26 @@ const swiper = new Swiper('.swiper', {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        dynamicBullets: true,
     },
 
+});
+
+const featuresSwiper = new Swiper(".features-swiper", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".features-swiper__pagination",
+        clickable: true,
+    },
+});
+
+const foodSwiper = new Swiper(".foods-swiper", {
+    slidesPerView: 1,       // Solo mostramos un grupo vertical a la vez
+    slidesPerGroup: 1,      // Avanzamos de a un grupo
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: '.foods-swiper__button-next',
+        prevEl: '.foods-swiper__button-prev',
+    },
 });
